d033d642d50752c3391e24664e0c802e44d18d9c
Russell Morland <rfmorland@gmail.com>
2019-01-25 13:14:30 +0100

    Update link.js

--- link.js
+++ link.js
@@ -1,6 +1,6 @@
 function run() {
     var html = document.getElementById('html');
-    var encoded = encodeURIcomponent(html);
+    var encoded = encodeURIComponent(html);
     var url = 'data:text/html,' + encoded;
     open(url, '_blank');
 }
