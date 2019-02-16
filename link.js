cb3ae4eb8b38b30a2714657d864b13420407deb5
Russell <you@example.com>
2019-02-16 17:13:20 +0100

    fix javascript link, and keep track of last used with cookies

--- link.js
+++ link.js
@@ -1,6 +1,41 @@
+var startCode = "<!DOCTYPE html>\n\
+<html>\n\
+  <head>\n\
+    <title>Hello World!</title>\n\
+  </head>\n\
+  <body>\n\
+    <h1>Hello World!</h1>\n\
+  </body>\n\
+</html>\n\
+";
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + encodeURIComponent(cvalue) + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var region = document.cookie.split(';');
+    for(var i = 0; i < region.length; i++) {
+	var c = region[i];
+	while (c.charAt(0) === ' ') {
+	    c = c.substring(1);
+	}
+	if (c.indexOf(name) === 0) {
+	    return decodeURIComponent(c.substring(name.length, c.length));
+	}
+    }
+    return null;
+}
+
 function run() {
-    var html = document.getElementById('html');
-    var encoded = encodeURIComponent(html);
-    var url = 'data:text/html,' + encoded;
-    window.open(url, '');
+    var html = document.getElementById('html').value;
+    setCookie('value', html);
+    document.write(html);
+    window.location.reload();
+}
+
+function setup() {
+    var cookieVal = getCookie('value');
+    document.getElementById('html').value = cookieVal===null ? startCode : cookieVal;
 }
